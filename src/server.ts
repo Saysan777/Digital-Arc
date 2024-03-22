0415d4afac6dd782e621262340fff4dff215fae7
Saysan777 <alokaryal32@gmail.com>
2024-03-22 21:03:15 +0545

    Fix: Solves a bug due to version issue in trpc and react-query

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 import express from 'express';
 import { getPayloadClient } from './get-payload';
 import { nextApp, nextHandler } from './next-utils';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { appRouter } from './trpc';
 
 const app = express();
 
 const PORT = Number(process.env.port) || 3000;
 
-const expressStartup = async () => {
-    const payload = await getPayloadClient({ initOptions:{ express: app, onInit: async (cms) => { cms.logger.info(`Admin URL ${ cms.getAdminURL() }`) } } });
+const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({ req, res });
 
-    app.use((req, res)=> nextHandler(req, res))
+const expressStartup = async () => {
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Admin URL ${ cms.getAdminURL() }`)
+                }
+        }
+    });
+
+    app.use('/api/trpc', trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }));
+
+    app.use((req, res)=> nextHandler(req, res));
 
     nextApp.prepare().then(()=> {
         payload.logger.info("NextJS server is running")
