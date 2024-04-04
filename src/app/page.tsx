cd57ac3a5f342cc0590f8cce45bfa0ee17304f92
Saysan777 <alokaryal32@gmail.com>
2024-04-04 21:02:34 +0545

    adding product reels procedures with zod validations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
           </div>
         </div>
 
-        <ProductReel title="Brand New" href="/products"/>
+        <ProductReel query={{ sort:'desc', limit: 4 }} title="Brand New" href="/products"/>
       </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
