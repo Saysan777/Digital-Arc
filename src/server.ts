cf4ef7aee51c28172e430aa8a8e12fa53adac0bb
Saysan777 <alokaryal32@gmail.com>
2024-04-23 20:36:03 +0545

    handling stripe webhook events

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,32 @@ import { nextApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
 import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
+import { stripeWebhookHandler } from './webhook';
+import nextBuild from 'next/dist/build'
+import path from 'path';
 
 const app = express();
 
-const PORT = Number(process.env.port) || 3000;
+const PORT = Number(process.env.port) || 4002;
 
 const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({ req, res });
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext >
 
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer }
+
+// Custom server creation in nextjs14. This is called custom server coz nextjs create backend api using api folder but we're using express.
 const expressStartup = async () => {
+    const webhookMiddleware = bodyParser.json({
+        verify: (req: WebhookRequest, _, buffer) => {
+            req.rawBody = buffer;
+        }
+    })
+
+    app.post('api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler)
+
     const payload = await getPayloadClient({
         initOptions: {
             express: app,
@@ -22,6 +38,17 @@ const expressStartup = async () => {
                 }
         }
     });
+     
+    if(process.env.NEXT_BUILD) {
+        app.listen(PORT, async ()=> {
+            payload.logger.info('Next.js is building for production');
+
+            // @ts-expect-error
+            await nextBuild(path.join(__dirname, '../'))
+
+            process.exit();
+        })
+    }
 
     //Making the trpc router a express server with this middelware and sending req back to nextjs(check folder api).
     app.use('/api/trpc', trpcExpress.createExpressMiddleware({
